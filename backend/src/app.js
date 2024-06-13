eda21913f81386cea6c5dc355e89594edf7f14ca
DaniSanchez0707 <danisanc927@gmail.com>
2024-06-12 20:35:07 -0600

    ⚡ Configuración CORS import
    ⚡ Configuración CORS import

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from '../src/routes/authentication.routes.js'
 import useragent from 'express-useragent'
 import dotenv from "dotenv"
+import cors from 'cors'
 
 dotenv.config()
 const reactPort = process.env.REACTPORT
@@ -18,7 +19,7 @@ app.use(cors({
     app.use(express.static('public'))
     app.use(useragent.express())
     app.use(express.urlencoded({extended: true}))
-    
+
 app.use('/api', authRoutes)
 
 export default app
