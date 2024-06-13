6b5ecb330c4113a8d984b694f7d547197688722e
Carlos <hcarlos780@gmail.com>
2024-06-12 21:44:54 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,20 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom' 
 import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 function App() {
 
   return (
-    <>
+    <> 
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
           <Route path="/home" element={<Home/>}/>
         </Routes>
       </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
