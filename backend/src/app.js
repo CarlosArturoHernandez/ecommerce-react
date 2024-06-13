0487557a746366aac751c1fdf3ff6a8cfb7ad800
DaniSanchez0707 <danisanc927@gmail.com>
2024-06-12 20:13:08 -0600

    Revert "⚡ Configuración CORS"
    This reverts commit b5c17aee163431d2437de066aed4643aae614d3d.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,4 @@
 import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-
-dotenv.config()
-const reactPort = process.env.REACTPORT
 const app = express()
-
-app.use(cors({
-origin: `https://localhost:${reactPort}`,
-credentials: true
-}))
-app.use(express.static('public'))
-app.use(useragent.express())
-app.use(express.urlencoded({extended: true}))
 
 export default app
