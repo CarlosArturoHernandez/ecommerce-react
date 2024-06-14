b1452a5bfed3bebdeb9ac3f5a70fb31b8d98aeea
Carlos A. <hcarlos780@gmail.com>
2024-06-14 17:44:02 -0600

    Features: 🐋 Docker with MariaDB

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,22 +1,20 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-dotenv.config()
-
+dotenv.config();
 
 export const connection = mysql.createConnection({
-host: process.env.MYSQL_HOST,
-user: process.env.MYSQL_USER,
-password: process.env.MYSQLDB_PASSWORD,
-database: process.env.MYSQL_DATABASE
-})
+  host: process.env.MARIADB_HOST,
+  user: process.env.MARIADB_USER,
+  password: process.env.MARIADB_PASSWORD,
+  database: process.env.MARIADB_DATABASE,
+  port: process.env.MARIADB_PORT,
+});
 
-
-export async function getConnection(){
-    connection.connect((err)=>{
-        if(err) return console.error('¡Error al conectarse a la base de datos!', err)
-        console.log('Conexión exitosa a la base de datos.')
-    })
+export async function getConnection() {
+  connection.connect((err) => {
+    if (err)
+      return console.error("¡Error al conectarse a la base de datos!", err);
+    console.log("Conexión exitosa a la base de datos.");
+  });
 }
-
-
