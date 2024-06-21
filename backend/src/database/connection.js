a4bd9780ebff05b852eb71727b7a90e29e0edc1d
DaniSanchez0707 <danisanc927@gmail.com>
2024-06-21 17:16:58 -0600

    ⚡️ Configuración Docker 🐋
    ⚡️ Configuración Docker 🐋

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,22 +1,19 @@
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-dotenv.config()
-
+dotenv.config();
 
 export const connection = mysql.createConnection({
-host: process.env.MYSQL_HOST,
-user: process.env.MYSQL_USER,
-password: process.env.MYSQLDB_PASSWORD,
-database: process.env.MYSQL_DATABASE
-})
+    host: process.env.MYSQL_HOST,
+    user: 'root',
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    port: process.env.MYSQL_DOCKER_PORT,
+    database:'ecommercedb'
+});
 
-
-export async function getConnection(){
-    connection.connect((err)=>{
-        if(err) return console.error('¡Error al conectarse a la base de datos!', err)
-        console.log('Conexión exitosa a la base de datos.')
-    })
+export async function getConnection() {
+    connection.connect((err) => {
+        if (err) return console.error('¡Error al conectarse a la base de datos!', err);
+        console.log('Conexión exitosa a la base de datos.');
+    });
 }
-
-
