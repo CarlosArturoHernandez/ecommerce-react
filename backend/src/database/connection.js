3884a522bb40c57987ffc7b4b3775017c6ade8cc
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-03 10:55:27 -0600

    Base de datos
    Base de datos

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,56 +1,27 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
-import { exec } from 'child_process';
-dotenv.config({ path: '../../../.env' });
 
-function iniciarServidorDeDesarrollo() {
-    exec('npm run dev', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error ejecutando npm run dev: ${error}`);
-            return;
-        }
-        console.log(`Servidor de desarrollo iniciado.`);
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
-}
+dotenv.config({ path: '../../../.env' });
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_LOCAL_PORT)
+console.log(process.env.DB_DATABASE)
 
 export const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_DOCKER_PORT,
-    database: process.env.DB_DATABASE
-});
+    host: 'localhost',  // Host de la base de datos
+    user: 'dani', // Usuario de la base de datos
+    password: '123', // Contraseña de la base de datos
+    database: 'ecommercedb' // Nombre de la base de datos a la que te conectas
+})
 
-export async function getConnection(intentos = 5) {
-    let intentosActuales = 0;
-    let conectado = false
 
-    while (intentosActuales < intentos && !conectado) {
-        try {
-            await new Promise((resolve, reject) => {
-                connection.connect((err) => {
-                    if (err) {
-                        console.log(`¡Error al conectarse a la base de datos, reintentando conexión... Intento ${intentosActuales + 1}/${intentos}`);
-                        intentosActuales++;
-                        if (intentosActuales === intentos) {
-                           
-                            iniciarServidorDeDesarrollo();
-                            reject(new Error('No se pudo conectar a la base de datos después de varios intentos.  Reiniciando servidor'));
-                        } else {
-                            setTimeout(resolve, 2000); // Esperar 2 segundos antes de reintentar
-                        }
-                    } else {
-                        conectado = true
-                        console.log('Conexión exitosa a la base de datos.');
-                        resolve();
-                    }
-                });
-            });
-        } catch (error) {
-            console.error('¡Error al conectarse a la base de datos después de varios intentos!', error);
-            throw error;
-        }
+export async function getConnection() {
+    try {
+        await connection.promise().connect();
+        console.log('Conexión exitosa a la base de datos.');
+    } catch (err) {
+        console.error('¡Error al conectarse a la base de datos!', err);
     }
 }
+
+export default connection;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://example.com/">
-        Your Websiteeeeeeeeeee
+        Your Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
