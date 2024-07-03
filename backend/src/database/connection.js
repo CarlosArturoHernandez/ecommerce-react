c32e64d71acb0362af26e54b170f3953254deccf
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-03 11:11:41 -0600

    Prueba
    Prueba

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,9 +2,10 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../../.env' });
+
 console.log(process.env.DB_USER)
 console.log(process.env.DB_PASSWORD)
-console.log(process.env.DB_LOCAL_PORT)
+console.log(process.env.DB_HOST)
 console.log(process.env.DB_DATABASE)
 
 export const connection = mysql.createConnection({
