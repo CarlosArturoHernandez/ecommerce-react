a49c71962e49dadbf8fa6ac5f070e33c789f2fb4
Carlos A. <hcarlos780@gmail.com>
2024-07-03 11:56:53 -0600

    Update en la ruta de variables de entorno connection.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,27 @@
-import express from "express"
-import authRoutes from '../src/routes/authentication.routes.js'
-import useragent from 'express-useragent'
-import dotenv from "dotenv"
-import cors from 'cors'
+import express from "express";
+import authRoutes from "../src/routes/authentication.routes.js";
+import useragent from "express-useragent";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config({ path: '../../.env' })
-const reactPort = process.env.REACT_LOCAL_PORT
+dotenv.config({ path: "C:/Users/Carlos/my-projects/ecommerce-react/.env" });
+const reactPort = process.env.REACT_LOCAL_PORT;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(useragent.express())
+app.use(express.json());
+app.use(useragent.express());
 
-app.use(cors({
+app.use(
+  cors({
     origin: `http://localhost:${reactPort}`,
-    credentials: true
-    }))
-    app.use(express.static('public'))
-    app.use(useragent.express())
-    app.use(express.urlencoded({extended: true}))
+    credentials: true,
+  })
+);
+app.use(express.static("public"));
+app.use(useragent.express());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', authRoutes)
+app.use("/api", authRoutes);
 
-export default app
+export default app;
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,28 +1,22 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-dotenv.config({ path: '../../../.env' });
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_DATABASE)
+dotenv.config({ path: "C:/Users/Carlos/my-projects/ecommerce-react/.env" });
 
 export const connection = mysql.createConnection({
-    host: process.env.DB_HOST,  // Host de la base de datos
-    user: process.env.DB_USER, // Usuario de la base de datos
-    password: process.env.PASSWORD, // Contraseña de la base de datos
-    database: process.env.DB_DATABASE // Nombre de la base de datos a la que te conectas
-})
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 export async function getConnection() {
-    try {
-        await connection.promise().connect();
-        console.log('Conexión exitosa a la base de datos.');
-    } catch (err) {
-        console.error('¡Error al conectarse a la base de datos!', err);
-    }
+  try {
+    await connection.promise().connect();
+    console.log("Conexión exitosa a la base de datos.");
+  } catch (err) {
+    console.error("¡Error al conectarse a la base de datos!", err);
+  }
 }
 
 export default connection;
